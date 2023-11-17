a646267faf69c620b15af9bce26b5c3c5b1e9411
Adry Mateo Ramon <77759876+Adrydevmateo@users.noreply.github.com>
2023-11-17 08:23:08 -0400

    Added ESLint and Prettier configuration

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,12 @@ function Greet(): void {
   console.log('hi')
 }
 
+const favoriteFruits: string[] = ['apple', 'strawberry', 'orange']
+
+function AddFruit(fruit: string): void {
+  favoriteFruits.push(fruit)
+}
+
 Greet()
+
+AddFruit(favoriteFruits[2])
